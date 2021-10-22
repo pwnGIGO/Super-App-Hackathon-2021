e844a8ce4193fa21b033ed6f6e9b4af385905f88
AnthonyFenrisulfr <65879340+AnthonyFenrisulfr@users.noreply.github.com>
2021-10-22 11:28:26 -0500

    Cabecera
    Creando componente cabecera

--- superAppBBVA/src/app/componentes/componentes.module.ts
+++ superAppBBVA/src/app/componentes/componentes.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CabeceraComponent } from './cabecera/cabecera.component';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
+
+import { CabeceraComponent } from './cabecera/cabecera.component';
 
 
 
