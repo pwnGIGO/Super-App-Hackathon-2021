825c8e84b7d4c9eb4dd55467a65af61c0d5fe00e
Felipe Téllez <luis.felipe_tc@live.com>
2021-10-22 11:47:38 -0500

    Servicios receiver y http

--- superAppBBVA/src/app/servicios/http.service.ts
+++ superAppBBVA/src/app/servicios/http.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  rutaLocal = "assets/Archivos JSON/";
+  rutaRemota = ""; //POR DEFINIR
+
+  
+  obtenerJSONlocal(json: string) {
+    return this.http.get(this.rutaLocal + json);
+  }
+
+  obtenerJSONremoto(url: string) {
+    return this.http.get(this.rutaRemota + url);
+  }
 }
--- superAppBBVA/src/app/servicios/receiver-json.service.ts
+++ superAppBBVA/src/app/servicios/receiver-json.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {  BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReceiverJsonService {
+  private listSource = new BehaviorSubject<any[]>([]);
+  
+  $getListSource = this.listSource.asObservable();
 
   constructor() { }
+  
+  sendListSource(list: any[]){
+    this.listSource.next(list);
+  }
 }
