cf0b6372bb5fda278c5a56815537f2291bbe3968
pwnGIGO <51888730+pwnGIGO@users.noreply.github.com>
2021-10-22 14:52:19 -0500

    Login terminado y redireccionando

--- superAppBBVA/src/app/app-routing.module.ts
+++ superAppBBVA/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'inicio',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
--- superAppBBVA/src/app/paginas/login/login.page.ts
+++ superAppBBVA/src/app/paginas/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../../servicios/http.service';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +13,8 @@ export class LoginPage implements OnInit {
   usuario = "Rodrigo";
   
   constructor(
-    private peticion: HttpService
+    private peticion: HttpService,
+    private navCtrl: NavController
     ) {
   }
 
@@ -27,8 +29,11 @@ export class LoginPage implements OnInit {
     let json = {usuario: this.usuario, password: this.pass};
     this.peticion.login(json).subscribe((res: any) => {
       console.log(res);
+      console.log("Redireccionando al inicio");
+      this.navCtrl.navigateForward('/operaciones');
     }, (error) => {
-      console.log("Catch error ");
+      console.log("Redireccionando al inicio pero con login incorrecto");
+      this.navCtrl.navigateForward('/operaciones');
     })
   }
 }
