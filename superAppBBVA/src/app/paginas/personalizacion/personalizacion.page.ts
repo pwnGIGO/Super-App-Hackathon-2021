5da0e849f0fb0d0dd50c18f261448d676851a7c7
pwnGIGO <51888730+pwnGIGO@users.noreply.github.com>
2021-10-23 16:43:43 -0500

    Logo

--- superAppBBVA/src/app/paginas/personalizacion/personalizacion.page.ts
+++ superAppBBVA/src/app/paginas/personalizacion/personalizacion.page.ts
@@ -9,26 +9,26 @@ import { AlertController, NavController } from '@ionic/angular';
 export class PersonalizacionPage implements OnInit {
 
   slides: {img:string, titulo:string, desc:string}[] =[
-    {
-      img:'assets/imagenes/Slides/3.svg',
-      titulo: 'slide 1',
-      desc: ''
-    },
-    {
-      img:'assets/imagenes/Slides/3.svg',
-      titulo: 'slide 2',
-      desc: ''
-    },
-    {
-      img:'assets/imagenes/Slides/3.svg',
-      titulo: 'slide 3',
-      desc: ''
-    },
-    {
-      img:'assets/imagenes/Slides/3.svg',
-      titulo: 'slide 4',
-      desc: ''
-    }
+  {
+    img:'assets/imagenes/Slides/3.svg',
+    titulo: 'slide 1',
+    desc: ''
+  },
+  {
+    img:'assets/imagenes/Slides/3.svg',
+    titulo: 'slide 2',
+    desc: ''
+  },
+  {
+    img:'assets/imagenes/Slides/3.svg',
+    titulo: 'slide 3',
+    desc: ''
+  },
+  {
+    img:'assets/imagenes/Slides/3.svg',
+    titulo: 'slide 4',
+    desc: ''
+  }
   ];
 
 
@@ -48,20 +48,24 @@ export class PersonalizacionPage implements OnInit {
     this.navCtrl.navigateForward('/operaciones');
   }
 
+  buscar($event){
+    console.log($event.target.value);
+  }
+
   async confirmaEnvio() {
-      const alertEnvio = await this.alertCtrl.create({
-        cssClass: 'alertEnvioExitoso',
-        backdropDismiss: false,
-        header: '¡Solicitud exitosa!',
-        message: 'Revisaremos tu solicitud y te notificaremos',
-        buttons: [
-        {
-          text: 'Cerrar',
-          role: 'cancel',
-        },
-        ],
-      });
-      await alertEnvio.present();
-    }
+    const alertEnvio = await this.alertCtrl.create({
+      cssClass: 'alertEnvioExitoso',
+      backdropDismiss: false,
+      header: '¡Solicitud exitosa!',
+      message: 'Revisaremos tu solicitud y te notificaremos',
+      buttons: [
+      {
+        text: 'Cerrar',
+        role: 'cancel',
+      },
+      ],
+    });
+    await alertEnvio.present();
+  }
 
 }
