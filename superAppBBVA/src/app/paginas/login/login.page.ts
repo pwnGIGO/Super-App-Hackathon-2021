d9b08e78394b38979556f473fff646f829d14a37
AnthonyFenrisulfr <65879340+AnthonyFenrisulfr@users.noreply.github.com>
2021-10-23 14:21:23 -0500

    Login
    Se termina vista del login

--- superAppBBVA/src/app/paginas/login/login.page.ts
+++ superAppBBVA/src/app/paginas/login/login.page.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 import { HttpService } from '../../servicios/http.service';
 import { NavController } from '@ionic/angular';
 
@@ -8,33 +14,55 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  @ViewChild('contenido', { read: ElementRef }) cont: ElementRef;
 
-  pass = "";
-  usuario = "Rodrigo";
-  
+  pass = '';
+  esPass = 'password';
+  usuario = 'Rodrigo';
+  mostrar = false;
+  expandido = true;
+
   constructor(
     private peticion: HttpService,
-    private navCtrl: NavController
-    ) {
-  }
+    private navCtrl: NavController,
+    private renderer: Renderer2
+  ) { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  expandeLogin() {
+    this.renderer.setStyle(this.cont.nativeElement, 'padding', '5em 3em');
   }
 
   guardaValores(valor) {
     this.pass = valor.detail.value;
   }
 
-  enviaDatos(){
-    console.log("Entre")
-    let json = {usuario: this.usuario, password: this.pass};
-    this.peticion.login(json).subscribe((res: any) => {
-      console.log(res);
-      console.log("Redireccionando al inicio");
-      this.navCtrl.navigateForward('/operaciones');
-    }, (error) => {
-      console.log("Redireccionando al inicio pero con login incorrecto");
-      this.navCtrl.navigateForward('/operaciones');
-    })
+  /*
+    Esta función muestra y oculta la contraseña que ingresa el usuario en el input.
+  */
+  muestraPass() {
+    if (this.esPass === 'password') {
+      this.esPass = 'text';
+    } else {
+      this.esPass = 'password';
+    }
+    this.mostrar = !this.mostrar;
+  }
+
+  enviaDatos() {
+    console.log('Entre');
+    let json = { usuario: this.usuario, password: this.pass };
+    this.peticion.login(json).subscribe(
+      (res: any) => {
+        console.log(res);
+        console.log('Redireccionando al inicio');
+        this.navCtrl.navigateForward('/operaciones');
+      },
+      (error) => {
+        console.log('Redireccionando al inicio pero con login incorrecto');
+        this.navCtrl.navigateForward('/operaciones');
+      }
+    );
   }
 }
